60110dba1204e7d39b981411f49bb86f74ecfffc
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-16 18:04:21 +0100

    Minor cleanup, recursing in dir WIP.

--- code.fs
+++ code.fs
@@ -1,12 +1,11 @@
 
-2variable statsfile
-"todostats.csv" statsfile 2!
+512 constant MAX-FSNAME-LENGTH ( We run into trouble if the filenames are longer than 512 chars )
+
+MAX-FSNAME-LENGTH buffer: filename-buffer ( Buffer which we re-use for read-dir )
 
 : discard-n-chars ( addr1 u1 n -- addr2 u2 )
 	\ Increment addr1 by n and decrement u1 by n (discard beginning of string)
-	tuck - ( addr1 n u2 )
-	-rot + ( u2 addr2 )
-	swap ;
+	tuck - ( addr1 n u2 ) -rot + ( u2 addr2 ) swap ;
 
 : count-substr ( addr1 u1 addr2 u2 -- u3 ) recursive
 	{ a1 u1 a2 u2 }
@@ -14,17 +13,26 @@
 	\ in string specified by addr1 u1
 	a1 u1 a2 u2 search ( a3 u3 f )
 	if \ Found string, recurse on rest
-		u2 ( a3 u3 u2 )
-		discard-n-chars ( a4 u4 )
-		a2 u2 ( a4 u4 a2 u2 )
-		count-substr ( u )
-		1+
+		u2 discard-n-chars ( a4 u4 ) \ Remove match at beginning of rest of text
+		a2 u2 count-substr \ How many times a2 u2 occurs in rest of text
+		1+ ( u ) \ Plus the one we found first
 	else
-		2drop 0 ( 0 )
+		2drop 0 ( 0 ) \ String occurs zero times in text
 	endif ;
 
-: TODOs-in-file ( addr1 u1 -- u2 )
+: todos-in-file ( addr1 u1 -- u2 )
 	\ addr1 u1 is the filename, returns number of "TODO"s in the file
-	slurp-file ( addr2 u2 )
-	s" TODO"
-	count-substr ( u3 ) ;
+	slurp-file s" TODO" count-substr ;
+
+: read-dir-easy ( dirid -- addr1 u1 ior )
+	;
+
+\ TODO: Recursively search files in given directory
+: exec-on-files-in-dir ( addr1 u1 xt -- )
+	\ use execute to run xt
+	-rot ( xt addr1 u1 )
+	open-dir throw ( xt wdirid )
+
+;
+
+\ TODO: Statistics reporting from csv file
