78304e03a2bd37b9d8120e15171aa503a93e2c60
Logi E. <logi.eyjolfsson@gmail.com>
2024-12-18 15:35:38 +0100

    Add script entry point

--- test.fs
+++ test.fs
@@ -1,7 +1,5 @@
 
-\ This is a test file
-\ TODO: Add some tests
-\ TODO: Remove this todo
+\ TODO: Add some more tests
 
 :noname
 	s" Here there is a TODO, and another TODO"
@@ -11,5 +9,12 @@
 	drop ;
 execute
 
-: print cr type ;
-s" code" ' print walk-dir
+
+: print-todos ( addr1 u1 -- )
+	\ Print filepath along with the number of TODOs in the file
+	2dup todos-in-file -rot ( u2 addr1 u1 )
+	tuck type ( u2 u1 )
+	25 swap - .r     \ Print TODO number aligned at col 25 (subtract path len)
+	cr
+;
+s" code" ' print-todos walk-dir
